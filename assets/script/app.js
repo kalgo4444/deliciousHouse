b42a84119a05cf7271568c95e3acd1803b9235f9
kalgo4444 <abuxyziabd@gmail.com>
2025-05-08 08:36:37 +0500

    first commit

---
+++ assets/script/app.js
@@ -0,0 +1,62 @@
+// ======================= h/w ========================
+{
+    // 1 - masala
+    // const findIndex = (arr, value) => arr.indexOf(value)
+    // console.log(findIndex([10, 7, 8, 55, 44], 8));
+}
+{
+    // 2 - masala 
+    // const addToEnd = (arr, value) => {
+    //     arr.push(value)
+    //     return arr
+    // }
+    // console.log(addToEnd([1, 2, 3], 5));
+}
+{
+    // 3 - masala 
+    // const addToEnd = arr => {
+    //     arr.pop()
+    //     return arr
+    // }
+    // console.log(addToEnd([1,2,3]));
+}
+{
+    // 4 - masala 
+    // const magreArrs = (arr1, arr2) => arr1.concat(arr2)
+    // console.log(magreArrs([1, 2, 3], [4, 5]));
+}
+{
+    // 5 - masala
+    // const sortArr = arr => arr.sort((a, b) => a - b)
+    // console.log(sortArr([2, 1, 3]));
+}
+{
+    // 6 - masala 
+    // const combineAndRev = (arr1, arr2) => arr1.concat(arr2).reverse()
+    // console.log(combineAndRev([1, 2], [4]));
+}
+{
+    // 7 - masala 
+    // const lastThreeRev = arr => arr.slice(-3).reverse().join()
+    // console.log(lastThreeRev([1, 2, 3, 4, 5]));
+}
+{
+    // 8 - masala 
+    // const topThreeBig = (arr1, arr2) => arr1.concat(arr2).sort((a, b) => a - b).slice(-3)
+    // console.log(topThreeBig([1, 7, 3], [5, 9, 2]));
+}
+// =================== qoshimcha dars payti berilgan ====================
+{
+    // telegram avatarkasiga oxshatip ismlani 1 - harfini chiqarish 
+    // function avart(str) {
+    //     let r = str.split(" ")
+    //     if (r.length >= 3 && r.length) {
+    //         return (r[0][0] + r[1][0] + r[2][0]).toUpperCase()
+    //     } else if (r.length == 2) {
+    //         return (r[0][0] + r[1][0]).toUpperCase()
+    //     } else if (r.length == 1) {
+    //         return (r[0][0]).toUpperCase()
+    //     }
+    // }
+    // console.log(avart("abdulaziz abdugafurov abdujalil o'gli")); // -> AAA
+}
